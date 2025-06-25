bd8eaf9137f49bd280df71075a45e25b48705851
alisapyak <komatsu_9416@mail.ru>
2025-06-25 08:11:18 +0500

    Add Home Page and Search Tests

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,10 +1,84 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
     private WebDriver driver;
 
+    @FindBy(id = "origin")
+    WebElement fromInput;
+
+    @FindBy(id = "destination")
+    WebElement toInput;
+
+    @FindBy(id = "date-picker-outbound-date")
+    WebElement departureDateInput;
+
+    @FindBy(id = "oneWayCheckbox")
+    WebElement oneWayCheckbox;
+
+    @FindBy(id = "searchFlights")
+    WebElement searchButton;
+
+    @FindBy(css = "#flight-search-error")
+    WebElement errorMessage;
+
+    @FindBy(id = "passengers")
+    WebElement passengersInput;
+
+    @FindBy(id = "cabinClass")
+    WebElement classInput;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setFrom(String from) {
+        fromInput.clear();
+        fromInput.sendKeys(from);
+    }
+
+    public void setTo(String to) {
+        toInput.clear();
+        toInput.sendKeys(to);
+    }
+
+    public void setDepartureDate(String date) {
+        departureDateInput.clear();
+        departureDateInput.sendKeys(date);
+        departureDateInput.sendKeys(Keys.ENTER);
+    }
+
+    public void toggleOneWay(boolean on) {
+        if (oneWayCheckbox.isSelected() != on) {
+            oneWayCheckbox.click();
+        }
+    }
+
+    public void setPassengers(String value) {
+        passengersInput.sendKeys(value);
+    }
+
+    public void setClass(String value) {
+        classInput.sendKeys(value);
+    }
+
+    public void clickSearch() {
+        searchButton.click();
+    }
+
+    public boolean isSearchEnabled() {
+        return searchButton.isEnabled();
+    }
+
+    public boolean isErrorDisplayed() {
+        try {
+            return errorMessage.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+}
--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,8 +1,41 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
 public class SearchTests extends BaseTest {
+
+    @Test
+    public void testSuccessfulFlightSearch_TC001() {
+        HomePage home = new HomePage(driver);
+        home.setFrom("Dublin");
+        home.setTo("Amsterdam");
+        home.setDepartureDate("25-07-2025");
+        home.clickSearch();
+        Assert.assertTrue(driver.getCurrentUrl().contains("search"), "Search result page not loaded.");
+    }
+
+    @Test
+    public void testEmptyFromField_TC002() {
+        HomePage home = new HomePage(driver);
+        home.setTo("Amsterdam");
+        home.setDepartureDate("25-07-2025");
+        home.clickSearch();
+        Assert.assertTrue(home.isErrorDisplayed() || !home.isSearchEnabled(), "Error message or disabled button expected.");
+    }
+
+    @Test
+    public void testOneWaySearch_TC004() {
+        HomePage home = new HomePage(driver);
+        home.setFrom("Dublin");
+        home.setTo("Amsterdam");
+        home.setDepartureDate("25-07-2025");
+        home.toggleOneWay(true);
+        home.clickSearch();
+        Assert.assertTrue(driver.getCurrentUrl().contains("search"), "One-way search failed.");
+    }
+}
+
 
