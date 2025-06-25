a97e114a3fe8706bdae2ba0ed9ec1a9b4a9d60d1
alisapyak <komatsu_9416@mail.ru>
2025-06-25 08:15:15 +0500

    Add Button State Test

--- src/test/java/tests/ButtonStateTests.java
+++ src/test/java/tests/ButtonStateTests.java
@@ -1,4 +1,20 @@
 package tests;
 
-public class ButtonStateTests {
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class ButtonStateTests extends BaseTest {
+
+    @Test
+    public void testSearchButtonState_TC005() {
+        HomePage home = new HomePage(driver);
+        Assert.assertFalse(home.isSearchEnabled(), "Search button should be disabled initially.");
+        home.setFrom("Dublin");
+        Assert.assertFalse(home.isSearchEnabled(), "Search button should still be disabled.");
+        home.setTo("Amsterdam");
+        home.setDepartureDate("25-07-2025");
+        Assert.assertTrue(home.isSearchEnabled(), "Search button should be enabled after filling all fields.");
+    }
 }
