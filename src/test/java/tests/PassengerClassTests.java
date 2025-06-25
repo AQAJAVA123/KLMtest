833780b0f377a5318d06ab1a0809c8ca6eef8ca8
alisapyak <komatsu_9416@mail.ru>
2025-06-25 08:52:58 +0500

    Updated and fixed PassClassTests

--- src/test/java/tests/PassengerClassTests.java
+++ src/test/java/tests/PassengerClassTests.java
@@ -8,24 +8,32 @@ import pages.HomePage;
 
 public class PassengerClassTests extends BaseTest {
 
-    @DataProvider(name = "pairwiseData")
-    public Object[][] providePairwiseCombinations() {
-        return new Object[][] {
-                {"1", "Economy"},
-                {"2", "Business"},
-                {"3", "Premium Comfort"},
+    @DataProvider(name = "passengerClassCombinations")
+    public Object[][] providePassengerClassCombinations() {
+        return new Object[][]{
+                {1, 0, 0, "Economy", "TC008_OneAdult_Economy"},
+                {2, 0, 0, "Business", "TC008_TwoAdults_Business"},
+                {2, 1, 0, "Premium Comfort", "TC008_Family_PremiumComfort"},
+                {1, 0, 1, "Economy", "TC008_AdultWithInfant"},
+                {2, 2, 1, "Business", "TC008_LargeFamily_Business"}
         };
     }
 
-    @Test(dataProvider = "pairwiseData")
-    public void testPassengerAndClassCombinations_TC008(String passengers, String cabinClass) {
+    @Test(dataProvider = "passengerClassCombinations")
+    public void testPassengerAndClassCombinations_TC008(int adults, int children, int infants, String cabinClass, String testCaseName) {
         HomePage home = new HomePage(driver);
-        home.setFrom("Dublin");
-        home.setTo("Amsterdam");
-        home.setDepartureDate("25-07-2025");
-        home.setPassengers(passengers);
+
+        home.setFrom("Dublin (DUB)");
+        home.setTo("Amsterdam (AMS)");
+        home.setDepartureDate("2025-07-25");
+
+        home.setPassengers(adults, children, infants);
         home.setClass(cabinClass);
+
         home.clickSearch();
-        Assert.assertTrue(driver.getCurrentUrl().contains("search"), "Search with passengers + class failed.");
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("search"),
+                "Test " + testCaseName + " failed: Search with " + adults + " adults, "
+                        + children + " children, " + infants + " infants in " + cabinClass + " class.");
     }
 }
