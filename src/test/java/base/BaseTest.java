cb7347ed2ef226c4c148978f9d9088c83973a71c
alisapyak <komatsu_9416@mail.ru>
2025-06-25 07:58:38 +0500

    Add base test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,11 +2,34 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
 import java.time.Duration;
 
 public class BaseTest {
-
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
+    @BeforeClass
+    public void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
 
+    @BeforeMethod
+    public void setupTest() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.klm.ie/");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
